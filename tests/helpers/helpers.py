9707ec5ad70cec0fe7443f0d7be771219d7b0590
Artem Malyshev <proofit404@gmail.com>
2020-09-05 23:11:34 +0300

    test: validate tox environments #148
    Deny tox generative and factor environments.  Do not install test
    helpers inside environments.

--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -1,7 +1,4 @@
-import configparser
 import functools
-import re
-import textwrap
 
 
 def is_not_empty(f):
@@ -16,22 +13,3 @@ def is_not_empty(f):
         assert count > 0
 
     return wrapper
-
-
-def tox_info(var):
-    """Get variable value from all sections in the tox.ini file."""
-    ini_parser = configparser.ConfigParser()
-    ini_parser.read("tox.ini")
-    for section in ini_parser:
-        if var in ini_parser[section]:
-            value = textwrap.dedent(ini_parser[section][var].strip())
-            yield section, value
-
-
-def tox_parse_envlist(string):
-    """Parse tox environment list with proper comma escaping."""
-    escaped = string
-    while re.search(r"({[^,}]*),", escaped):
-        escaped = re.subn(r"({[^,}]*),", r"\1:", escaped)[0]
-    parts = escaped.split(",")
-    return [re.subn(r":", ",", p)[0].strip() for p in parts]
