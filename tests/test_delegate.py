1e72b6b17ba433ad27060d592dd2c16b71baefcc
Artem Malyshev <proofit404@gmail.com>
2022-10-16 20:00:19 +0300

    test: define @delegate specs #236

--- tests/test_delegate.py
+++ tests/test_delegate.py
@@ -1,14 +1,13 @@
 """Tests related to the @delegated decorator."""
 from datetime import date
 
-from generics import delegated
 from generics import private
 
 
 def test_define_instance_methods(e, w):
     """Provide access to not redefined methods."""
     user_class = private(e.User)
-    smart_user_class = delegated(user_class)(w.SmartUser)
     user = user_class(last_login=date(1999, 12, 31))
-    smart_user = smart_user_class(user)
+    smart_user = w.SmartUser(user)
     assert not smart_user.is_active()
+    assert smart_user.was_called()
